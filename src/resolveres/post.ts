9d5063b030aa92913ca28634b63c4653fa3e42dc
LexiosAlex <lexiosma@gmail.com>
2020-09-30 01:54:53 +0300

    ApolloServer: added common resolvers

--- src/resolveres/post.ts
+++ src/resolveres/post.ts
@@ -1,18 +1,55 @@
-import {Arg, Ctx, Int, Query, Resolver} from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Post } from "../enteties/Post";
 import { MyContext } from "../types";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() ctx: MyContext): Promise<Post[]> {
-    return ctx.em.find(Post, {});
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    return em.find(Post, {});
   }
 
-  @Query(() => Post, {nullable: true})
+  @Query(() => Post, { nullable: true })
   post(
-    @Arg('id', () => Int) id: number,
-    @Ctx() ctx: MyContext): Promise<Post | null> {
-    return ctx.em.findOne(Post, {id});
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id });
+    return true
   }
 }
